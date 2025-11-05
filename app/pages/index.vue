1f46e2b17923011c83507328fd713b8cdeb4d5d4
peausr114 <nanthawut.mue@pea.co.th>
2025-11-05 13:30:46 +0700

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+const cards = ref([
+  {
+    title: 'ผปว.กบฟ.',
+    description: 'การปฏิบัติงานด้านบัญชีและการเงินภายในองค์กรและใช้ T-Code ต่างๆ เพื่อดำเนินการในระบบเช่น การตั้งหนี้, การกลับรายการเอกสาร, การเคลียร์เงินยืมพนักงาน, และการจัดการเช็คโดยมี ผปว.(แผนกประมวลผลและวิเคราะห์บัญชี)เป็นหน่วยงานหลักที่รับผิดชอบ',
+    icon: 'arcticons:debtcalc',
+    to: 'https://qr.pea.co.th/ODc4YzRkM',
+    src: '/account.png',
+  },
+  {
+    title: 'กบพ.',
+    description: 'ข้อมูลด้านพัสดุ',
+    icon: 'healthicons:rdt-result-out-stock',
+    to: ' https://qr.pea.co.th/ZjAxM2050',
+    src: '/warehouse.png',
+  },
+  {
+    title: 'ผขฟ.กบฟ.',
+    description: 'ด้านลูกหนี้ค่าไฟ การออกบิล',
+    icon: 'material-symbols:article-person-outline',
+    to: 'https://qr.pea.co.th/Zml4OGNjM',
+    src: '/electricity-bill.png'
+  },
+  {
+    title: 'ผสซ.ฝสบ.',
+    description: 'ด้านโปรแกรมและแอปพลิเคชั่นภายในองค์กร',
+    icon: 'streamline-pixel:computers-devices-electronics-desktop',
+    to: ' https://qr.pea.co.th/M2VjNWNKY',
+    src: '/computer-graphic.png'
+  }
+])
+
+</script>
+
+
+<template>
+  <div>
+    <UPageSection
+      id="features"
+      title="CHATBOT ฝสบ.กฟน3"
+      description="นายเมธี บุญพรหม ผชก.(น3) ผลักดันให้นำเทคโนโลยี AI มาใช้งานในหน่วยงาน กฟน.3 โดยมี นายธรรมนูญ สุวรรณพฤกษ์ อฝ.สบ. ได้ดำเนินการตามนโยบาย โดยมีการจัดทำ Chatbot เพื่อนำมาอำนวยความสะดวกให้กับพนักงานในการตอบปัญหางานในส่วนต่าง"
+    >
+    <UPageGrid>
+    <UPageCard
+      v-for="(card, index) in cards"
+      :key="index"
+      v-bind="card"
+      highlight
+      highlight-color="primary"
+    >
+     <NuxtImg :src="card.src" class="size-25" />
+    </UPageCard>
+    </UPageGrid>
+  </UPageSection>
+    <!-- <UPageSection
+      id="features"
+      title="CHATBOT ฝสบ.กฟน3"
+      description="นาย เมธี บุญพรหม ผชก.(น3) ผลักดันให้นำเทคโนโลยี AI มาใช้งานในหน่วยงาน กฟน.3 โดยมี นายธรรมนูญ สุวรรณพฤกษ์ อฝ.สบ. ได้ดำเนินการตามนโยบาย โดยมีการจัดทำ Chatbot เพื่อนำมาอำนวยความสะดวกให้กับพนักงานในการตอบปัญหางานในส่วนต่าง"
+      :features="[{
+        icon: 'i-lucide-palette',
+        title: 'Production-ready from day one',
+        description: 'Pre-configured with TypeScript, ESLint, Tailwind CSS, and all the best practices. Focus on building features, not setting up tooling.'
+      }, {
+        icon: 'i-lucide-palette',
+        title: 'Beautiful by default',
+        description: 'Leveraging Nuxt UI\'s design system with automatic dark mode, consistent spacing, and polished components that look great out of the box.'
+      }, {
+        icon: 'i-lucide-zap',
+        title: 'Lightning fast',
+        description: 'Optimized for performance with SSR/SSG support, automatic code splitting, and edge-ready deployment. Your users will love the speed.'
+      }, {
+        icon: 'i-lucide-blocks',
+        title: '100+ components included',
+        description: 'Access Nuxt UI\'s comprehensive component library. From forms to navigation, everything is accessible, responsive, and customizable.'
+      }, {
+        icon: 'i-lucide-code-2',
+        title: 'Developer experience first',
+        description: 'Auto-imports, hot module replacement, and TypeScript support. Write less boilerplate and ship more features.'
+      }, {
+        icon: 'i-lucide-shield-check',
+        title: 'Built for scale',
+        description: 'Enterprise-ready architecture with proper error handling, SEO optimization, and security best practices built-in.'
+      }]"
+    /> -->
+  </div>
+</template>
+
+
+<style>
+.custom-icon-color  {
+  size: 1000pm;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,35 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  nitro: { preset: 'static' },
+  ssr: false,
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image'],
+  colorMode: {
+    preference: "light",
+    storageKey: 'custom-key-color-mode'
+  },
+
+  devtools: {
+    enabled: true
+  },
+  image: {
+    dir: 'assets/img',
+    provider: 'ipx',
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
